7bb44d18803c9bebf11d2bd32ab70324444165e1
Matea Howard <bl0wfacee@gmail.com>
2023-01-02 15:55:50 +0000

    adding deploy.sh

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/toy-store/",
+  build: {
+    outDir: "./docs",
+  },
 });
